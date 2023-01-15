0b78b36686814fc405abf115f7b7d55e86028e15
kadiravcii <116902375+kadiravcii@users.noreply.github.com>
2023-01-15 14:57:28 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,29 @@
+let namePrompt = prompt("Adınızı Giriniz", );
+let nameSpan = document.querySelector('#myName');
+
+
+if(namePrompt <= 0 || namePrompt >= 1  ){
+    alert('Boş giriş yaptınız')
+    location.reload();
+  
+}else{
+    
+}
+nameSpan.innerHTML=`${namePrompt}`
+
+const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    function showTime() {
+    let dateTime = new Date();
+    let day = days[dateTime.getDay()];
+    let hour = dateTime.getHours();
+    let minute = dateTime.getMinutes();
+    let second = dateTime.getSeconds();
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    second = (second < 10) ? "0" + second : second;
+
+    document.querySelector("#myClock").innerHTML = `${hour}: ${minute}: ${second}  ${day}`
+}
+setInterval(showTime, 1000);
+showTime();
